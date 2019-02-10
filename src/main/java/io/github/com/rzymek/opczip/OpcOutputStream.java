137adbfbb7f24587606299d3fb4e7fd6c050b0cd
Krzysztof Rzymkowski <rzymek@gmail.com>
2019-02-10 20:48:08 +0100

    working implementation

--- src/main/java/io/github/com/rzymek/opczip/OpcOutputStream.java
+++ src/main/java/io/github/com/rzymek/opczip/OpcOutputStream.java
@@ -1,29 +1,98 @@
 package io.github.com.rzymek.opczip;
 
+import io.github.com.rzymek.opczip.Zip64Impl.Entry;
+
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.charset.Charset;
-import java.util.zip.ZipEntry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.*;
 
-public class OpcOutputStream extends OutputStream {
+public class OpcOutputStream extends DeflaterOutputStream {
+
+    private final Zip64Impl spec;
+    private final List<Entry> entries = new ArrayList<>();
+    private Entry current;
+    private CRC32 crc = new CRC32();
+    private int written = 0;
+    private boolean finished = false;
 
     public OpcOutputStream(OutputStream out) {
+        super(out, new Deflater(Deflater.DEFAULT_COMPRESSION, true));
+        this.spec = new Zip64Impl(out);
     }
 
-    public OpcOutputStream(OutputStream out, Charset charset) {
-    }
-
+    /**
+     * @see Deflater#setLevel(int)
+     */
     public void setLevel(int level) {
+        super.def.setLevel(level);
     }
 
+    /**
+     * @see java.util.zip.ZipOutputStream#putNextEntry(ZipEntry)
+     */
     public void putNextEntry(ZipEntry e) throws IOException {
+        if (current != null) {
+            closeEntry();
+        }
+        current = new Entry(e.getName());
+        current.offset = written;
+        written += spec.writeLFH(current);
+        entries.add(current);
     }
 
+    /**
+     * @see ZipOutputStream#closeEntry()
+     */
     public void closeEntry() throws IOException {
+        if (current == null) {
+            throw new IllegalStateException("not current zip current");
+        }
+        def.finish();
+        while (!def.finished()) {
+            deflate();
+        }
+
+        current.size = def.getBytesRead();
+        current.compressedSize = (int) def.getBytesWritten();
+        current.crc = crc.getValue();
+
+        written += current.compressedSize;
+        written += spec.writeDAT(current);
+        current = null;
+        def.reset();
+        crc.reset();
+    }
+
+    public void finish() throws IOException {
+        if(finished){
+            return;
+        }
+        if(current != null) {
+            closeEntry();
+        }
+        int offset = written;
+        for (Entry entry : entries) {
+            written += spec.writeCEN(entry);
+        }
+        spec.writeEND(entries.size(), offset, written - offset);
+        finished = true;
+    }
+
+    public synchronized void write(byte[] b, int off, int len) throws IOException {
+        if (off < 0 || len < 0 || off > b.length - len) {
+            throw new IndexOutOfBoundsException();
+        } else if (len == 0) {
+            return;
+        }
+        super.write(b, off, len);
+        crc.update(b, off, len);
     }
 
     @Override
-    public void write(int b) throws IOException {
-
+    public void close() throws IOException {
+        finish();
+        out.close();
     }
 }
--- src/test/java/io/github/com/rzymek/opczip/ZipCompatibilityTests.java
+++ src/test/java/io/github/com/rzymek/opczip/ZipCompatibilityTests.java
@@ -1,15 +1,20 @@
 package io.github.com.rzymek.opczip;
 
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.util.zip.ZipFile;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
@@ -17,37 +22,58 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class OpcOutputStreamTest {
+class ZipCompatibilityTests {
     private Map<String, String> contents = createContents();
 
     @Test
     void test() throws IOException {
-
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         create(out, contents);
         out.close();
 
-        byte[] zipFile = out.toByteArray();
-        assertThat(zipFile.length, greaterThan(0));
-        validate(new ByteArrayInputStream(zipFile), contents);
+        byte[] zipBytes = out.toByteArray();
+        assertThat(zipBytes.length, greaterThan(0));
+        validate(new ByteArrayInputStream(zipBytes));
+
+        Path file = Files.createTempFile(Paths.get("target"), "test", ".zip");
+        try {
+            Files.write(file, zipBytes);
+            validateZipFile(file.toFile());
+        }finally {
+            file.toFile().delete();
+        }
     }
 
-    private void validate(InputStream in, Map<String, String> contents) throws IOException {
-        ZipInputStream zip = new ZipInputStream(in);
+    private void validate(InputStream in) throws IOException {
+        ZipArchiveInputStream zip = new ZipArchiveInputStream(in);
         Set<String> leftToRead = new HashSet<>(contents.keySet());
         for (; ; ) {
-            ZipEntry entry = zip.getNextEntry();
+            ArchiveEntry entry = zip.getNextEntry();
             if (entry == null) {
                 break;
             }
             assertEquals(contents.get(entry.getName()), readFully(zip));
             assertTrue(leftToRead.remove(entry.getName()));
-            zip.closeEntry();
         }
         assertThat(leftToRead, empty());
     }
 
-    private void create(ByteArrayOutputStream out, Map<String, String> contents) throws IOException {
+    private void validateZipFile(File file) throws IOException {
+        ZipFile zipFile = new ZipFile(file);
+        Set<String> leftToRead = new HashSet<>(contents.keySet());
+        zipFile.stream().forEach(entry -> {
+            try {
+                InputStream in = zipFile.getInputStream(entry);
+                assertEquals(contents.get(entry.getName()), readFully(in));
+                assertTrue(leftToRead.remove(entry.getName()));
+            } catch (IOException ex) {
+                throw new UncheckedIOException(ex);
+            }
+        });
+        assertThat(leftToRead, empty());
+    }
+
+    private void create(OutputStream out, Map<String, String> contents) throws IOException {
         try (OpcOutputStream zip = new OpcOutputStream(out)) {
             for (Map.Entry<String, String> entry : contents.entrySet()) {
                 zip.putNextEntry(new ZipEntry(entry.getKey()));
@@ -66,9 +92,9 @@ class OpcOutputStreamTest {
 
     static Map<String, String> createContents() {
         Map<String, String> contents = new LinkedHashMap<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             String name = String.format("dir%s/file%s.txt", i % 3, i);
-            contents.put(name, "this is the contents of " + name);
+            contents.put(name, "this is the contents");
         }
         return contents;
     }
